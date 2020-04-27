8705f7e4f2d4deb135b4ac743b836cd52b9690e3
xuchao <xuchao@ksjgs.com>
2020-04-27 16:20:15 +0800

    first

---
+++ ip.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+import socket
+
+"""
+查询本机ip地址
+:return: ip
+"""
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+
+    return ip
+
+
+
+if __name__ == '__main__':
+    print("本机ip地址是:%s"%get_host_ip())
